05487732df1be7aaaa9017244d381ccd82859198
Andrei Phelps <LePinecone@gmail.com>
2024-12-11 15:50:39 -0700

    Working on backpropagation.

--- src/ConvolutionalNeuralNetwork.py
+++ src/ConvolutionalNeuralNetwork.py
@@ -1,24 +1,23 @@
 import numpy as np
 
-
 class ConvolutionalLayer:
     def __init__(self, filters, sizeOfKernal, theChannels, padding=0, input_data=0, stride=1):
-        self.filterQuantity = filters
-        self.filter_size = sizeOfKernal
-        self.input_data = input_data * 10
-        self.input_channels = theChannels
-        self.padding = padding
-        self.stride = stride
+        self.filterQuantity = filters  # number of filters
+        self.filter_size = sizeOfKernal  # filter height/width
+        self.input_data = input_data * 10  # store input data scaling
+        self.input_channels = theChannels  # input channels
+        self.padding = padding  # zero-padding size
+        self.stride = stride  # stride for convolution
         shapeForOurFilter = (filters, theChannels, sizeOfKernal, sizeOfKernal)
         filterVals = np.random.randn(*shapeForOurFilter)
-        filterVals = filterVals * 0.1
+        filterVals = filterVals * 0.1  # scale filters
         self.filters = filterVals
         zeroStart = np.zeros(filters)
         self.biases = zeroStart
 
     def dataPadding(self, dataToPad, padVal = 0):
         padVal = self.padding
-        dataToPad = dataToPad * 1
+        dataToPad = dataToPad * 1  # ensure copy
         if padVal <= 0:
             return dataToPad
         elif padVal == -1:
@@ -28,6 +27,7 @@ class ConvolutionalLayer:
         elif padVal == -10:
             return "Wayne Debugging - issue in Convolutional Layer padding"
         else:
+            # pad input data
             ThePadding = dataToPad
             newSize = self.padding
             dasPad = (newSize,newSize)
@@ -36,18 +36,14 @@ class ConvolutionalLayer:
                           (startingValPadding, startingValPadding, dasPad, dasPad),
                           )
 
-
-    def forward(self, input_data,padVal=0):
-
+    def forward(self, input_data, padVal=0):
         padVal = self.padding
-
         forwardData = self.input_data
-
         forwardData = self.dataPadding(input_data)
         dimensionsOfMatrix = forwardData.shape
 
-        batch_size = dimensionsOfMatrix[0]
-        depth = dimensionsOfMatrix[1]
+        batch_size = dimensionsOfMatrix[0]  # number of samples
+        depth = dimensionsOfMatrix[1]  # number of channels
         height = dimensionsOfMatrix[2]
         width = dimensionsOfMatrix[3]
 
@@ -59,7 +55,6 @@ class ConvolutionalLayer:
         strideAdd = 1
 
         height = (x // y) + strideAdd
-
         x1 = width - self.filter_size
         y1 = self.stride
 
@@ -67,11 +62,10 @@ class ConvolutionalLayer:
             pass
         else:
             outPutWidth = (x1 // y1) + 1
-
             zeroInput = (batch_size, self.filterQuantity, height, outPutWidth)
-
             output = np.zeros(zeroInput)
 
+            # convolve input with filters
             aRange = range(batch_size)
             bRange = range(self.filterQuantity)
             cRange = range(height)
@@ -80,22 +74,21 @@ class ConvolutionalLayer:
                 for b in bRange:
                     for c in cRange:
                         for d in dRange:
-                            # Extract the region of the input corresponding to the filter
                             cRowBegin = c * self.stride
                             cRowEnd = c * self.stride + self.filter_size
                             dRowBegin = d * self.stride
                             dRowEnd = d * self.stride + self.filter_size
                             convolutionArea = forwardData[
-                                     a, :,
-                                     cRowBegin:cRowEnd,
-                                     dRowBegin:dRowEnd
-                                     ]
+                                              a, :,
+                                              cRowBegin:cRowEnd,
+                                              dRowBegin:dRowEnd
+                                              ]
                             if convolutionArea is not None:
                                 total = 0
-                                for x in range(convolutionArea.shape[0]):
-                                    for y in range(convolutionArea.shape[1]):
-                                        for z in range(convolutionArea.shape[2]):
-                                            total += convolutionArea[x][y][z] * self.filters[b][x][y][z]
+                                for x_index in range(convolutionArea.shape[0]):
+                                    for y_index in range(convolutionArea.shape[1]):
+                                        for z_index in range(convolutionArea.shape[2]):
+                                            total += convolutionArea[x_index][y_index][z_index] * self.filters[b][x_index][y_index][z_index]
 
                                 output[a][b][c][d] = total + self.biases[b]
                             else:
@@ -112,20 +105,81 @@ class ConvolutionalLayer:
                             else:
                                 print("We have an error in ConVLayer")
 
+            # store for backward
+            self.last_input = input_data
+            self.last_output = output
+
             return output
+
+    def backward(self, d_out):
+        # backprop for convolution
+        batch_size = d_out.shape[0]
+        filt_count = self.filterQuantity
+        padded_input = self.dataPadding(self.last_input)
+        padded_input_shape = padded_input.shape
+        in_channels = padded_input_shape[1]
+        out_height = d_out.shape[2]
+        out_width = d_out.shape[3]
+
+        # ReLU gradient
+        relu_mask = (self.last_output > 0).astype(float)
+        d_out = d_out * relu_mask
+
+        d_filters = np.zeros_like(self.filters)
+        d_biases = np.zeros_like(self.biases)
+        d_input_padded = np.zeros_like(padded_input)
+
+        # compute gradients for filters, biases, input
+        for a in range(batch_size):
+            for b in range(filt_count):
+                for c in range(out_height):
+                    for d_i in range(out_width):
+                        h_start = c * self.stride
+                        h_end = h_start + self.filter_size
+                        w_start = d_i * self.stride
+                        w_end = w_start + self.filter_size
+
+                        region = padded_input[a, :, h_start:h_end, w_start:w_end]
+                        d_filters[b] += region * d_out[a, b, c, d_i]
+                        d_biases[b] += d_out[a, b, c, d_i]
+
+                        # accumulate gradient to input
+                        for x_i in range(region.shape[0]):
+                            for y_i in range(region.shape[1]):
+                                for z_i in range(region.shape[2]):
+                                    d_input_padded[a, x_i, h_start+y_i, w_start+z_i] += self.filters[b, x_i, y_i, z_i] * d_out[a, b, c, d_i]
+
+        # remove padding from input gradient
+        if self.padding > 0:
+            d_input = d_input_padded[:, :, self.padding:-self.padding, self.padding:-self.padding]
+        else:
+            d_input = d_input_padded
+
+        self.d_filters = d_filters
+        self.d_biases = d_biases
+
+        return d_input
+
+    def update_params(self, learning_rate):
+        # update filters and biases
+        for i in range(self.filters.shape[0]):
+            for j in range(self.filters.shape[1]):
+                for k in range(self.filters.shape[2]):
+                    for l in range(self.filters.shape[3]):
+                        self.filters[i, j, k, l] -= learning_rate * self.d_filters[i, j, k, l]
+
+        for i in range(self.biases.shape[0]):
+            self.biases[i] -= learning_rate * self.d_biases[i]
 
 
 class MaxPoolingLayer:
     def __init__(self, pool_size, stride):
-        self.pool_size = pool_size
-        self.stride = stride
+        self.pool_size = pool_size  # pooling window size
+        self.stride = stride  # stride for pooling
 
     def forward(self, input_data, maxPoolingPadding=0):
-
         maxPoolingPadding = maxPoolingPadding
-
         forwardData = input_data
-
         forwardData = input_data
         dimensionsOfMatrix = forwardData.shape
 
@@ -142,7 +196,6 @@ class MaxPoolingLayer:
         strideAdd = 1
 
         height = (x // y) + strideAdd
-
         x1 = width - self.pool_size
         y1 = self.stride
 
@@ -150,21 +203,22 @@ class MaxPoolingLayer:
             pass
         else:
             outPutWidth = (x1 // y1) + 1
-
             zeroInput = (batch_size, depth, height, outPutWidth)
-
             output = np.zeros(zeroInput)
+
+            # store max indices for backward
+            self.argmax_positions = np.zeros((batch_size, depth, height, outPutWidth, 2), dtype=int)
 
             aRange = range(batch_size)
             bRange = range(depth)
             cRange = range(height)
             dRange = range(outPutWidth)
 
+            # find max in each pooling region
             for a in aRange:
                 for b in bRange:
                     for c in cRange:
                         for d in dRange:
-                            # Extract the region of the input corresponding to the pooling area
                             cRowBegin = c * self.stride
                             cRowEnd = c * self.stride + self.pool_size
                             dRowBegin = d * self.stride
@@ -176,18 +230,48 @@ class MaxPoolingLayer:
                                           ]
 
                             if poolingArea is not None:
-                                maxVal = -10000  # Start with a very small number
+                                maxVal = -10000
+                                max_x = 0
+                                max_y = 0
                                 xMax = range(poolingArea.shape[0])
                                 yMax = range(poolingArea.shape[1])
-                                for x in xMax:
-                                    for y in yMax:
-                                        if poolingArea[x][y] > maxVal:
-                                            maxVal = poolingArea[x][y]
+                                for x_index in xMax:
+                                    for y_index in yMax:
+                                        if poolingArea[x_index][y_index] > maxVal:
+                                            maxVal = poolingArea[x_index][y_index]
+                                            max_x = x_index
+                                            max_y = y_index
                                 output[a][b][c][d] = maxVal
+                                self.argmax_positions[a, b, c, d] = (cRowBegin + max_x, dRowBegin + max_y)
                             else:
                                 print("ERROR IN poolingLayer max pooling!")
 
+            self.last_input = input_data
+            self.last_output = output
+
             return output
+
+    def backward(self, d_out):
+        # backprop through max pooling
+        batch_size = d_out.shape[0]
+        depth = d_out.shape[1]
+        out_height = d_out.shape[2]
+        out_width = d_out.shape[3]
+
+        d_input = np.zeros_like(self.last_input)
+        # distribute gradients back to where max was found
+        for a in range(batch_size):
+            for b in range(depth):
+                for c in range(out_height):
+                    for d_i in range(out_width):
+                        (h_idx, w_idx) = self.argmax_positions[a, b, c, d_i]
+                        d_input[a, b, h_idx, w_idx] += d_out[a, b, c, d_i]
+
+        return d_input
+
+    def update_params(self, learning_rate):
+        # no params in max pooling
+        pass
 
 
 class FullyConnectedLayer:
@@ -208,40 +292,106 @@ class FullyConnectedLayer:
         forwardBias = self.biases
         output += forwardBias
 
-        # Replace np.maximum with a custom ReLU implementation
+        # ReLU activation
         reluOutput = output.copy()
         xMax = range(reluOutput.shape[0])
         yMax = range(reluOutput.shape[1])
-        for x in xMax:
-            for y in yMax:
-                if reluOutput[x][y] < 0:
+        for x_i in xMax:
+            for y_i in yMax:
+                if reluOutput[x_i][y_i] < 0:
                     if reluOutput is not None:
-                        reluOutput[x][y] = 0
+                        reluOutput[x_i][y_i] = 0
                     else:
                         print("ERROR in FCL, ConNeuralNetwork")
 
+        self.last_input = input_data
+        self.last_output = reluOutput
         return reluOutput
+
+    def backward(self, d_out):
+        # backprop through fully connected layer
+        relu_mask = (self.last_output > 0).astype(float)
+        d_out = d_out * relu_mask
+
+        d_weights = np.zeros_like(self.weights)
+        d_biases = np.zeros_like(self.biases)
+        d_input = np.zeros_like(self.last_input)
+
+        # gradients wrt weights, bias
+        for i in range(d_out.shape[0]):
+            for j in range(d_out.shape[1]):
+                d_biases[j] += d_out[i][j]
+                for k in range(self.weights.shape[0]):
+                    d_weights[k][j] += self.input_data[i][k] * d_out[i][j]
+
+        # gradients wrt input
+        for i in range(d_out.shape[0]):
+            for k in range(self.weights.shape[0]):
+                val = 0
+                for j in range(self.weights.shape[1]):
+                    val += d_out[i][j] * self.weights[k][j]
+                d_input[i][k] = val
+
+        self.d_weights = d_weights
+        self.d_biases = d_biases
+        return d_input
+
+    def update_params(self, learning_rate):
+        # update weights and biases
+        for i in range(self.weights.shape[0]):
+            for j in range(self.weights.shape[1]):
+                self.weights[i][j] -= learning_rate * self.d_weights[i][j]
+
+        for i in range(self.biases.shape[0]):
+            self.biases[i] -= learning_rate * self.d_biases[i]
 
 
 class LeNet5:
     def __init__(self, input_shape, num_classes):
+        # create layers of LeNet5
         self.conv1 = ConvolutionalLayer(filters=6, sizeOfKernal=5, theChannels=input_shape[0], padding=2)
         self.pool1 = MaxPoolingLayer(pool_size=2, stride=2)
         self.conv2 = ConvolutionalLayer(filters=16, sizeOfKernal=5, theChannels=6)
         self.pool2 = MaxPoolingLayer(pool_size=2, stride=2)
 
-        # Correct the input size of fc1 to 576
+        # fc layers
         self.fc1 = FullyConnectedLayer(input_features=16 * 6 * 6, output_features=120)
         self.fc2 = FullyConnectedLayer(input_features=120, output_features=84)
         self.fc3 = FullyConnectedLayer(input_features=84, output_features=num_classes)
 
     def forward(self, input_data):
+        # forward pass through LeNet5
         x = self.conv1.forward(input_data)
         x = self.pool1.forward(x)
         x = self.conv2.forward(x)
         x = self.pool2.forward(x)
-        x = x.reshape(x.shape[0], -1)  # Flatten
+        x = x.reshape(x.shape[0], -1)  # flatten
         x = self.fc1.forward(x)
         x = self.fc2.forward(x)
-        output = self.fc3.forward(x)  # No activation for output
+        output = self.fc3.forward(x)  # output layer
+        self.last_input = input_data
+        self.last_output = output
         return output
+
+    def backward(self, d_out, learning_rate=0.01):
+        # backprop through entire network
+        d_out = self.fc3.backward(d_out)
+        d_out = self.fc2.backward(d_out)
+        d_out = self.fc1.backward(d_out)
+
+        batch_size = d_out.shape[0]
+        d_out = d_out.reshape(batch_size, 16, 6, 6)
+
+        d_out = self.pool2.backward(d_out)
+        d_out = self.conv2.backward(d_out)
+        d_out = self.pool1.backward(d_out)
+        d_out = self.conv1.backward(d_out)
+
+        # update all learnable parameters
+        self.conv1.update_params(learning_rate)
+        self.conv2.update_params(learning_rate)
+        self.fc1.update_params(learning_rate)
+        self.fc2.update_params(learning_rate)
+        self.fc3.update_params(learning_rate)
+
+        return d_out
